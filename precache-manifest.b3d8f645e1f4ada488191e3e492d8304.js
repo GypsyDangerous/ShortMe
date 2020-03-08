99b1bf20095a2c854ca347e9f96ea4f38a8efd67
David Snyder <davidgraygs4@gmail.com>
2020-03-08 15:38:43 -0400

    Updates

---
+++ precache-manifest.b3d8f645e1f4ada488191e3e492d8304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc2790a6d1cd37914d23643242c3541",
+    "url": "/ShortMe/index.html"
+  },
+  {
+    "revision": "4b93a941f9b68c4ca48f",
+    "url": "/ShortMe/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b2d820024091e993a7f0",
+    "url": "/ShortMe/static/css/main.e3688261.chunk.css"
+  },
+  {
+    "revision": "4b93a941f9b68c4ca48f",
+    "url": "/ShortMe/static/js/2.3c6bd8ee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ShortMe/static/js/2.3c6bd8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d820024091e993a7f0",
+    "url": "/ShortMe/static/js/main.68c11f3f.chunk.js"
+  },
+  {
+    "revision": "458502243f270b1dcc7f",
+    "url": "/ShortMe/static/js/runtime-main.2814c062.js"
+  }
+]);
